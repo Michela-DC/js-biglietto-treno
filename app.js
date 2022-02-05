74714517519d32306aec1e78d3bde249149477bc
Michela-DC <micheladelconte@outlook.com>
2022-02-05 22:54:10 +0100

    Added correction

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 - va applicato uno sconto del 20% per i minorenni
-- va applicato uno sconto del 40% per gli over 65. 
+- va applicato uno sconto del 40% per gli over 65.
 
 L’esercizio richiede di fare un minimo di controlli sui dati inseriti dall’utente.
 L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).*/
@@ -31,19 +31,21 @@ if (userAge > 1) {
 
 // Calcolo prezzo biglietto pieno oppure scontato
 const fullTicketPrice = (userKm * 0.21);
-// console.log(fullTicketPrice);
 
 if (userAge < 18){
     let discountedTicket = ( (fullTicketPrice * 20) / 100 );
     console.log(`Prezzo biglietto scontato = ${discountedTicket.toFixed(2)} €`);
 
 } else if (userAge > 65){
-    let discountedTicket = ( (fullTicketPrice * 40) / 100 );
+    discountedTicket = ( (fullTicketPrice * 40) / 100 );
     console.log(`Prezzo biglietto scontato = ${discountedTicket.toFixed(2)} €`);
 
 } else{
     console.log(`Prezzo biglietto = ${fullTicketPrice.toFixed(2)} €`);
 }
+
+
+
 
 
 

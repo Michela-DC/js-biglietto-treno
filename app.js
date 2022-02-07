310e9af47cbf9cc6d5f7f7a9f1cf95635fae8ac2
Michela-DC <micheladelconte@outlook.com>
2022-02-07 01:04:12 +0100

    Added html output

--- app.js
+++ app.js
@@ -8,42 +8,62 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 L’esercizio richiede di fare un minimo di controlli sui dati inseriti dall’utente.
 L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).*/
 
-// chiedo km da percorrere e verifico che abbia scritto in numero
-const userKm = parseFloat( prompt ('Ciao! Quanti chilometri devi percorrere?') );
+
+// chiedo km da percorrere, verifico che abbia scritto in numero e stampo su console e html
+let userKm = parseFloat( prompt ('Ciao! Quanti chilometri devi percorrere?') );
 console.log(`Chilometri da percorre = ${userKm.toFixed(2)} km`);
 
 if ( isNaN(userKm) ){
     alert('Devi inserire i chilometri in numero, grazie!');
 }
 
-// chiedo età e verifico che abbia scritto in numero
+const HTMLkm = document.getElementById('km');
+
+HTMLkm.innerHTML += ` ${userKm.toFixed(2)} Km`;
+
+
+// chiedo età, verifico che abbia scritto in numero e stampo su console e html
 const userAge = parseInt( prompt('Quanti anni hai?') );
 
 if ( isNaN(userAge) ){
     alert('Devi inserire la tua età in numero, grazie!');
 }
 
+const HTMLage = document.getElementById('age')
+
 if (userAge > 1) {
     console.log(`L'età del passeggero è di ${userAge} anni`);
+
+    HTMLage.innerHTML += ` ${userAge} anni`;
 } else{
     console.log(`L'età del passeggero è di ${userAge} anno`);
+
+    HTMLage.innerHTML += ` ${userAge} anno`;
 }
 
-// Calcolo prezzo biglietto pieno oppure scontato
+
+// Calcolo prezzo biglietto pieno oppure scontato e stampo su console e html
 const fullTicketPrice = (userKm * 0.21);
+
+let HTMLticketPrice = document.getElementById('ticket-price');
 
 if (userAge < 18){
     let discountedTicket = ( (fullTicketPrice * 20) / 100 );
     console.log(`Prezzo biglietto scontato = ${discountedTicket.toFixed(2)} €`);
 
+    HTMLticketPrice.innerHTML += ` ${discountedTicket.toFixed(2)} €`;
+
 } else if (userAge > 65){
     discountedTicket = ( (fullTicketPrice * 40) / 100 );
     console.log(`Prezzo biglietto scontato = ${discountedTicket.toFixed(2)} €`);
 
+    HTMLticketPrice.innerHTML += ` ${discountedTicket.toFixed(2)} €`;
+
 } else{
     console.log(`Prezzo biglietto = ${fullTicketPrice.toFixed(2)} €`);
-}
 
+    HTMLticketPrice.innerHTML += ` ${fullTicketPrice.toFixed(2)} €`
+}
 
 
 
